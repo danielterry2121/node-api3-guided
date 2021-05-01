8587bb124940af73657c3f212109c303e1d16eaa
Daniel Terry <daniel.terry2121@gmail.com>
2021-05-01 14:08:31 -0400

    guided project dealing with middleware and way to apply it in our servers

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express'); // importing a CommonJS module
-
+const morgan = require('morgan')
+const helmet = require('helmet')
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+//creating own middleware
+const logQuote= (coin) => (req,res,next) => {
 
+  console.log(`a ${coin} earned is a ${coin} not enjoyed :) `)
+  console.log(req.method, req.url)
+  next()
+
+}
+
+
+server.use(express.json());
+//gives details about request use -D for deveelopment
+server.use(morgan("dev"))
+//protect headers
+server.use(helmet())
+//self built 
+server.use(logQuote("nickel"))
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
